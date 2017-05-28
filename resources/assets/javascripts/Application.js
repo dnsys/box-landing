d1988cb4356b99569e121ecfcf7ba40f195b1a3e
dnnsys <as@rsdigital.ru>
2017-05-28 17:58:53 +0300

    added calculator

--- resources/assets/javascripts/Application.js
+++ resources/assets/javascripts/Application.js
@@ -6,48 +6,51 @@ class Application{
         console.log('application start');
         document.addEventListener('DOMContentLoaded', () => {
              console.log('application ready');
-             this._rangeSquareInit();
-             this._rangeTimeRent();
+             this._calculatorInit();
         })
     }
 
-    _rangeSquareInit() {
+    _calculatorInit() {
         let cost;
+        let month;
+        let sum;
+        let imgContainerSrc;
+        let imgContainer = $('.calculator__image-container img');
+        //let irsSingle = $('.calculator__range-group--square .irs-single');
         $('#squareInput').ionRangeSlider({
-            //type: "single",
-            //min: 1,
-            //max: 10,
+            type: "single",
             values: ['1m²', '6m²', '9m²', '12m²', '35m²'],
             from: 0,
             step: 1,
             grid: true,
             grid_snap: true,
             onStart: function (data) {
-                $('span.square').text(data.from);
                 switch(data.from){
-                    case 0: cost = 1000; break;
-                    case 1: cost = 2000; break;
-                    case 2: cost = 3000; break;
-                    case 3: cost = 4000; break;
-                    case 4: cost = 5000; break;
+                    case 0: cost = 1000; imgContainerSrc = '1m'; break;
+                    case 1: cost = 2000; imgContainerSrc = '6m'; break;
+                    case 2: cost = 3000; imgContainerSrc = '9m'; break;
+                    case 3: cost = 4000; imgContainerSrc = '12m'; break;
+                    case 4: cost = 5000; imgContainerSrc = '35m'; break;
                 }
-                console.log(cost);
+                sum = cost * month;
+                $('.calculator__output span').text(sum);
+                imgContainer.attr('src', './images/containers/'+imgContainerSrc+'.png');
             },
             onChange: function (data) {
-                $('span.square').text(data.from);
-                let cost;
                 switch(data.from){
-                    case 0: cost = 1000; break;
-                    case 1: cost = 2000; break;
-                    case 2: cost = 3000; break;
-                    case 3: cost = 4000; break;
-                    case 4: cost = 5000; break;
+                    case 0: cost = 1000; imgContainerSrc = '1m'; break;
+                    case 1: cost = 2000; imgContainerSrc = '6m'; break;
+                    case 2: cost = 3000; imgContainerSrc = '9m'; break;
+                    case 3: cost = 4000; imgContainerSrc = '12m'; break;
+                    case 4: cost = 5000; imgContainerSrc = '35m'; break;
                 }
-                console.log(cost);
+                sum = cost * month;
+                $('.calculator__output span').text(sum);
+                imgContainer.attr('src', './images/containers/'+imgContainerSrc+'.png');
+                let descr = $('.calculator__range-group--square .irs-single').text();
+                $('.calculator__square-descr span').text(descr);
             }
         });
-    }
-    _rangeTimeRent() {
         $('#timeRent').ionRangeSlider({
             type: "single",
             min: 1,
@@ -57,10 +60,14 @@ class Application{
             grid: true,
             grid_snap: true,
             onStart: function (data) {
-                $('span.time').text(data.from);
+                month = data.from;
+                sum = cost * month;
+                $('.calculator__output span').text(sum);
             },
             onChange: function (data) {
-                $('span.time').text(data.from);
+                month = data.from;
+                sum = cost * month;
+                $('.calculator__output span').text(sum);
             }
         });
     }
